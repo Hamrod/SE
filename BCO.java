ac449faba27bcf399effe029c37cc0c7a54cb729
Luca <luca.beurel@gmail.com>
2020-11-21 18:00:18 +0100

    Ajout constructeur sur BCO

--- BCO.java
+++ BCO.java
@@ -1,5 +1,25 @@
 public class BCO extends binMeta {
 
+    int nbBee;
+    int nbMoveBee;
+
+    public BCO(Data startPoint,Objective obj,long maxTime) {
+        try {
+            String msg = "Impossible de créer l'objet BCO : ";
+            if (maxTime <= 0) throw new Exception(msg + "le temps d'exec mac est inf a 0");
+            this.maxTime = maxTime;
+            if (startPoint == null) throw new Exception(msg + "le StartPoint est null");
+            this.solution = startPoint;
+            if (obj == null) throw new Exception(msg + "l'objectif est null");
+            this.obj = obj;
+            this.objValue = this.obj.value(this.solution);
+            this.metaName = "BCO";
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
     @Override
     public void optimize() {
 
--- RandomWalk.java
+++ RandomWalk.java
@@ -9,8 +9,7 @@
  */
 
 import java.util.Random;
-import java.util.List;
-import java.util.ArrayList;
+
 
 public class RandomWalk extends binMeta
 {
