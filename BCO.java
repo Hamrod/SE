118dced9356232a8892b7d3e3404be9c017da91e
Malo <moy_97@hotmail.fr>
2020-11-21 19:41:50 +0100

    Début des test de BCO

--- BCO.java
+++ BCO.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BCO extends binMeta {
@@ -20,6 +22,7 @@ public class BCO extends binMeta {
             this.objValue = this.obj.value(this.solution);
             this.metaName = "BCO";
 
+            bees = new ArrayList<>();
             for (int i = 0 ; i < nbBee ; i++) {
                 bees.add(new Bee(startPoint, obj, maxTime, nc));
             }
@@ -31,6 +34,34 @@ public class BCO extends binMeta {
 
     @Override
     public void optimize() {
+        System.out.println(bees);
+        for (Bee bee : bees) {
+            bee.optimize();
+        }
+        Collections.sort(bees, (b1, b2) -> {
+            return Double.compare(obj.value(b1.solution), obj.value(b2.solution));
+        });
+        System.out.println(bees);
+        this.solution = bees.get(0).solution;
+    }
 
+    // main
+    public static void main(String[] args) {
+        int ITMAX = 1000;  // number of iterations
+        int BEESNUMBER = 100;  // number of bees
+        int NC = 5;
+
+        // BitCounter
+        int n = 50;
+        Objective obj = new BitCounter(n);
+        Data D = obj.solutionSample();
+        BCO bco = new BCO(D, obj, ITMAX, BEESNUMBER, NC);
+        System.out.println(bco);
+        System.out.println("starting point : " + bco.getSolution());
+        System.out.println("optimizing ...");
+        bco.optimize();
+        System.out.println(bco);
+        System.out.println("solution : " + bco.getSolution());
+        System.out.println();
     }
 }
